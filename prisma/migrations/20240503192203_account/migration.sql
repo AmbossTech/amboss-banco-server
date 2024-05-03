9d2c68461312250b04bf0bd210d631710c639ac2
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-05-03 21:29:49 +0200

    chore: add prisma

---
+++ prisma/migrations/20240503192203_account/migration.sql
@@ -0,0 +1,16 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+-- CreateTable
+CREATE TABLE "account" (
+    "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "email" VARCHAR NOT NULL,
+    "password_hint" VARCHAR NOT NULL,
+    "master_password_hash" VARCHAR NOT NULL,
+    "symmetric_key_iv" VARCHAR NOT NULL,
+    "protected_symmetric_key" VARCHAR NOT NULL,
+    "key_pair" JSONB NOT NULL,
+
+    CONSTRAINT "account_pkey" PRIMARY KEY ("id")
+);
