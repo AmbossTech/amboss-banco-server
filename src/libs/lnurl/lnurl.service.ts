5a841ecad4463fef3d17cf1c32d18974c5b2b4de
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-07-09 10:42:24 -0500

    Revert "chore: temp switch to non covenant swap"
    This reverts commit 622fd0c053b9935059ba3dcce99a627b47099ae1.

--- src/libs/lnurl/lnurl.service.ts
+++ src/libs/lnurl/lnurl.service.ts
@@ -281,7 +281,7 @@ export class LnurlService {
             address.address().toString(),
             checkAmount,
             walletAcc.id,
-            true,
+            false,
           );
         },
       ],
