35b1b7449b0790f3364c7c7c4894f3e82a952ee7
Anthony Potdevin <potdevin.anthony@gmail.com>
2024-07-29 16:40:33 -0500

    chore: date fields

--- prisma/migrations/20240729214006_2fa/migration.sql
+++ prisma/migrations/20240729214006_2fa/migration.sql
@@ -4,9 +4,11 @@ CREATE TYPE "two_fa_method" AS ENUM ('OTP', 'PASSKEY');
 -- CreateTable
 CREATE TABLE "account_2fa" (
     "id" UUID NOT NULL DEFAULT uuid_generate_v4(),
+    "created_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "account_id" UUID NOT NULL,
     "method" "two_fa_method" NOT NULL,
-    "enabled" BOOLEAN NOT NULL DEFAULT false,
+    "enabled" BOOLEAN NOT NULL DEFAULT true,
     "payload" JSONB NOT NULL,
 
     CONSTRAINT "account_2fa_pkey" PRIMARY KEY ("id")
