30d605f2b24ca557bf42303db1d160f8ae3989df
Anthony Potdevin <31413433+apotdevin@users.noreply.github.com>
2024-08-14 19:14:50 -0600

    [AMB-208] feat: swap table (#47)
    * feat: swap table
    
    * chore: more stuff

--- src/api/swaps/swaps.resolver.ts
+++ src/api/swaps/swaps.resolver.ts
@@ -1,7 +1,47 @@
 import { Parent, ResolveField, Resolver } from '@nestjs/graphql';
+import { wallet_account_swap } from '@prisma/client';
 import { SwapsRepoService } from 'src/repo/swaps/swaps.repo';
 
-import { WalletSwaps, WalletSwapsParent } from './swaps.types';
+import { SimpleSwap, WalletSwaps, WalletSwapsParent } from './swaps.types';
+import {
+  getSwapDepositAmount,
+  getSwapFrom,
+  getSwapSettleAmount,
+  getSwapTo,
+} from './swaps.utils';
+
+@Resolver(SimpleSwap)
+export class SimpleSwapResolver {
+  @ResolveField()
+  created_at(@Parent() { created_at }: wallet_account_swap) {
+    return created_at.toISOString();
+  }
+
+  @ResolveField()
+  provider(@Parent() { request }: wallet_account_swap) {
+    return request.provider;
+  }
+
+  @ResolveField()
+  deposit_coin(@Parent() { request, response }: wallet_account_swap) {
+    return getSwapFrom(request, response);
+  }
+
+  @ResolveField()
+  settle_coin(@Parent() { request, response }: wallet_account_swap) {
+    return getSwapTo(request, response);
+  }
+
+  @ResolveField()
+  deposit_amount(@Parent() { request, response }: wallet_account_swap) {
+    return getSwapDepositAmount(request, response);
+  }
+
+  @ResolveField()
+  settle_amount(@Parent() { request, response }: wallet_account_swap) {
+    return getSwapSettleAmount(request, response);
+  }
+}
 
 @Resolver(WalletSwaps)
 export class WalletSwapsResolver {
